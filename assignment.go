1fe7fc57d61af8396568e4a6a71fcdc0e8508a74
Raj Patel <rajpatel17398@gmail.com>
2022-02-08 14:57:32 -0600

    first commit

---
+++ assignment.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"vs_code/testP2"
+	"vs_code/testPackage"
+
+	"github.com/uniplaces/carbon"
+)
+
+func main() {
+	fmt.Println("Hellooo")
+	fmt.Println(testPackage.Cal(2, 3))
+	fmt.Println(testP2.Sub(5, 1))
+
+	fmt.Printf("Right now is %s\n", carbon.Now().DateTimeString())
+
+}
